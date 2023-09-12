6ed19c738c6830da27a16598fc1d8dfb04a0cb33
Ahmed <dosunmuahmed240@gmail.com>
2023-09-12 06:14:10 +0100

    made final changes to the task

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const slackUserName = document.querySelector('[data-testid="slackUserName"]');
     const currentDayOfTheWeek = document.querySelector('[data-testid="currentDayOfTheWeek"]');
     const currentUTCTime = document.querySelector('[data-testid="currentUTCTime"]');
+    const myTrack = document.querySelector('[data-testid="myTrack"]');
+    const githubURL = document.querySelector('[data-testid="githubURL"]');
 
     // Update Slack name
     slackUserName.textContent = "Ahmed Dosunmu";
@@ -13,7 +15,20 @@ document.addEventListener("DOMContentLoaded", function() {
     const dayOfWeek = daysOfWeek[currentDate.getUTCDay()];
     currentDayOfTheWeek.textContent = dayOfWeek;
 
-    // Update current UTC time
-    const utcTime = currentDate.getTime();
-    currentUTCTime.textContent = `Current UTC Time: ${utcTime} ms`;
+    // Function to update UTC time
+    function updateUTCTime() {
+        const currentTime = new Date();
+        const utcTime = currentTime.getTime();
+        currentUTCTime.textContent = `Current UTC Time: ${utcTime} ms`;
+
+        // Update the time every 1000ms (1 second)
+        setTimeout(updateUTCTime, 1000);
+    }
+
+    // Call the function to start updating UTC time
+    updateUTCTime();
+
+    // Update track and GitHub URL
+    myTrack.textContent = "My track: Frontend";
+    githubURL.href = "https://github.com/Ay-Dee/zuri-frontend-task-1";
 });
